c255d6a583552648e2b35a666ccb7d22b50d8caa
Akhilesh KUmar <akhilesh.h.kumar@accenture.com>
2020-03-10 11:52:21 +0530

    String Joiner Test1

---
+++ StringJoinerTest.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class StringJoinerTest 
+{
+
+	public static void main(String[] args) 
+	{
+		StringJoiner sjoin = new StringJoiner(",");
+		
+		sjoin.add("Kamalnath");
+		sjoin.add("Sindhiya");
+		sjoin.add("Shivraj");
+		
+		System.out.println(sjoin);
+
+	}
+
+}
