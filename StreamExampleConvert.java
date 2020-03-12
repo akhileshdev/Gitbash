b502341ad26bc58fa4fd7b0fdb8c4b4f7456e204
Akhilesh KUmar <akhilesh.h.kumar@accenture.com>
2020-03-12 17:12:41 +0530

    Stream API conversion to Map

--- StreamExampleConvert.java
+++ StreamExampleConvert.java
@@ -32,11 +32,15 @@ class StreamExampleConvert
     	  list.add(new Product(5,"Oneplus",33000f));
     	  list.add(new Product(6,"IPhone",60000f));
     	  
-    	  Set<Float> set = list.stream().map(prc->prc.price).collect(Collectors.toSet());
-    	  
+    	  Set<Float> set = list.stream().map(prc->prc.price).collect(Collectors.toSet()); //converting list into set using Stream API method   	  
     	  System.out.println("\n\nSet representation of Price : "+set);
 		  
 		  //System.out.println("Stream Example concise and compact code with Stream API");
+    	  
+    	  Map<Integer,String> map = list.stream().collect(Collectors.toMap(p->p.id,p->p.name));
+    	  System.out.println("Map Representation : "+map);
+    	  
+    	  
     	  
       }
 }
